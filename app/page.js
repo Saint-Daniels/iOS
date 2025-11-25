61f0f90c36fc57c400f033609392c8487649ffe0
Saint Daniels <saintdaniels@healthcare.com>
2025-11-25 03:51:02 +0100

    Update Privacy & Data Protections section to use cat.jpeg image

--- app/page.js
+++ app/page.js
@@ -519,8 +519,8 @@ export default function Home() {
                 <ScrollSlideIn direction="left">
                   <div className="service-image-wrapper">
                     <Image
-                      src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?auto=format&fit=crop&w=1000&q=80"
-                      alt="Data security and privacy protection"
+                      src="/images/cat.jpeg"
+                      alt="Data security and privacy protection - vintage illustration of privacy and protection"
                       width={600}
                       height={400}
                       className="service-image"
